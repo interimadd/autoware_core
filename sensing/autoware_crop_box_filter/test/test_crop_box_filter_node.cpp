4061dbad62bd961f61249c3cd7c9965cefb1c049
Takahisa.Ishikawa <takahisa.ishikawa@tier4.jp>
2025-08-18 15:14:13 +0900

    test(crop_box_filter): add test for filtering with zero input points
    Signed-off-by: Takahisa.Ishikawa <takahisa.ishikawa@tier4.jp>

--- sensing/autoware_crop_box_filter/test/test_crop_box_filter_node.cpp
+++ sensing/autoware_crop_box_filter/test/test_crop_box_filter_node.cpp
@@ -80,6 +80,59 @@ bool is_same_points(
   return true;
 }
 
+TEST(CropBoxFilterTest, FilterZeroPointReturnZeroPoint)
+{
+  // parameters for crop box filter
+  const double min_x = -5.0;
+  const double max_x = 5.0;
+  const double min_y = -5.0;
+  const double max_y = 5.0;
+  const double min_z = -5.0;
+  const double max_z = 5.0;
+  const bool negative = true;
+
+  // input points
+  std::vector<std::array<float, 3>> input_points = {};
+
+  // expected points after filtering
+  std::vector<std::array<float, 3>> expected_points = {};
+
+  rclcpp::NodeOptions node_options;
+  const int64_t max_queue_size = 5;
+  node_options.parameter_overrides({
+    {"min_x", min_x},
+    {"min_y", min_y},
+    {"min_z", min_z},
+    {"max_x", max_x},
+    {"max_y", max_y},
+    {"max_z", max_z},
+    {"negative", negative},
+    {"input_pointcloud_frame", "base_link"},
+    {"input_frame", "base_link"},
+    {"output_frame", "base_link"},
+    {"max_queue_size", max_queue_size},
+  });
+
+  // Create the node with the specified options
+  autoware::crop_box_filter::CropBoxFilter node(node_options);
+
+  // Create pointcloud using helper function
+  sensor_msgs::msg::PointCloud2 pointcloud = create_pointcloud2(input_points);
+
+  const sensor_msgs::msg::PointCloud2::ConstSharedPtr pointcloud_msg =
+    std::make_shared<sensor_msgs::msg::PointCloud2>(pointcloud);
+
+  auto output = sensor_msgs::msg::PointCloud2();
+  // filtering
+  node.filter_pointcloud(pointcloud_msg, output);
+
+  // Extract points from output using helper function
+  std::vector<std::array<float, 3>> output_points = extract_points_from_cloud(output);
+
+  // check if the points are inside/outside the box as expected
+  EXPECT_TRUE(is_same_points(expected_points, output_points));
+}
+
 TEST(CropBoxFilterTest, FilterExcludePointsInsideBoxWhenNegative)
 {
   // parameters for crop box filter
